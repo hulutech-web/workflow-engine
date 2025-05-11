3754de8fa5935fd04a2907626a1c7ba437935984
zhoudm1743 <zhoudm1743@163.com>
2025-05-11 14:44:41 +0800

    refactor(models): 将用户手机号字段类型从varchar(11)改为varchar(255)
    修改用户模型中的手机号字段类型，以支持更长的手机号输入，提高数据存储的灵活性

--- app/models/user.go
+++ app/models/user.go
@@ -5,7 +5,7 @@ import "github.com/hulutech-web/workflow-engine/pkg/plugin/types"
 type User struct {
 	Model
 	Username     string      `json:"username" gorm:"type:varchar(255);not null;comment:'用户名'"`
-	Phone        types.Phone `json:"phone" gorm:"type:varchar(11);comment:'手机号'"`
+	Phone        types.Phone `json:"phone" gorm:"type:varchar(255);comment:'手机号'"`
 	Password     string      `json:"password" gorm:"type:varchar(32);not null"`
 	Salt         string      `gorm:"not null;default:'';comment:'加密盐巴'"`
 	Email        string      `json:"email" gorm:"type:varchar(125);"`
