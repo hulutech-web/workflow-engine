3747606a13fa278ed2360851986832e8821edf78
hulutech-web <yuanhaozhuzhu@hotmail.com>
2025-05-13 02:20:57 +0800

    清理原组件

--- workflow-vue/src/plugins/terminal/index.ts
+++
@@ -1,6 +0,0 @@
-import Terminal from 'vue-web-terminal'
-import 'vue-web-terminal/lib/theme/dark.css'
-const setup = (app: App) => {
-    app.use(Terminal)
-}
-export { setup }
--- workflow-vue/src/routes/default.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  path: "/",
-  component: () => import("@/layouts/admin/index.vue"),
-  redirect: "/admin/dashboard",
-};
