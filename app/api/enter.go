8f3ffe3ea4782af0e0e971804714c5af33b11d8a
hulutech-web <yuanhaozhuzhu@hotmail.com>
2025-05-18 13:53:40 +0800

    eventbreak

--- app/api/enter.go
+++ app/api/enter.go
@@ -3,10 +3,14 @@ package api
 import (
 	"github.com/hulutech-web/workflow-engine/app/api/route"
 	"github.com/hulutech-web/workflow-engine/app/api/service"
+	"github.com/hulutech-web/workflow-engine/core/event"
+	"github.com/hulutech-web/workflow-engine/core/queue"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Options(
 	service.Module,
 	route.Module,
+	event.Module,
+	queue.Module,
 )
